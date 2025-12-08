e216452393b1564b6643e2f13e5cd8c472a4d4d6
dammy216 <dammy3612@icloud.com>
2025-12-08 13:08:58 +0000

    送信処理

--- raspberrypi/rpi.js
+++ raspberrypi/rpi.js
@@ -1,16 +1,23 @@
+import axios from "axios";
 import { Chip, Line } from "node-libgpiod";
 
 const readGPIO = () => {
   const chip = new Chip(0);
   const line = new Line(chip, 26);
+
   line.requestInputMode();
-
   const value = line.getValue();
-  line.release(); // ハンドルを解放
-  
+  line.release();
   return value;
-}
+};
 
-setInterval(() => {
-  console.log(readGPIO());
-}, 100);
+setInterval(async () => {
+  const value = readGPIO();
+
+  if (value === 1) {
+    await axios.post("http://119.105.96.137:3000/sensor", {
+      value: value,
+      detectedAt: new Date().toISOString()
+    });
+  }
+}, 300);
