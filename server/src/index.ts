b7b04ceb0141cdeed5285c224cf167958f1c594e
dammy216 <dammy3612@icloud.com>
2025-12-08 14:28:08 +0000

    ローカルホストを指すよう修正

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ app.route('/infrared', infrared);
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3000,
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`);
 });
