898ee8d96a75692dd5a65c05ab61cb4952f19ca2
dammy216 <dammy3612@icloud.com>
2025-12-08 23:41:44 +0900

    いらない処理を削除

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,12 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
-import posts from './routes/posts.js';
 import infrared from './routes/infrared.js';
 
 const app = new Hono();
 
 app.use('*', cors({ origin: '*', allowMethods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 
-app.route('/posts', posts);
 app.route('/infrared', infrared);  
 
 serve({
